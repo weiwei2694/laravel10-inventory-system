4431a3926fc9c192f3a84c60471e33d669b25a5b
Arya Priema <99aryaprima@gmail.com>
2024-01-31 23:41:11 +0700

    feat: create OrderItem model and order_items migration. Add one to many relationship between
    - Set the table name, primary key, increments and timestamp
    - Added quantity, unit_price, product_id and order_id fields
    - Add one to many relationship between OrderItem to Order and OrderItem to Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -25,5 +26,10 @@ class Product extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class, 'product_id', 'id');
     }
 }
