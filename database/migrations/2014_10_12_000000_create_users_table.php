278b89e435f7b49da5bab86a002569a18d7cdd91
Arya Priema <99aryaprima@gmail.com>
2024-02-01 18:16:13 +0700

    feat: create Role.php model and roles migration, changing the role field data type in users migration
    - Added name fields
    - Changed the role field type from string to integer, in users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('role')->nullable(false)->default(1);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('role')->nullable(false)->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
