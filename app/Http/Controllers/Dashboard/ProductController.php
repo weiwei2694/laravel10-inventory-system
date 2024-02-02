d7ee7e0424519aba9fc81397c6683af9acb39ad5
Arya Priema <99aryaprima@gmail.com>
2024-02-02 13:28:10 +0700

    feat: update existing product

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductStoreUpdateRequest;
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -35,28 +37,20 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store()
+    public function store(ProductStoreUpdateRequest $request): RedirectResponse
     {
-        request()->validate([
-            'name' => 'required|max:100',
-            'description' => 'required',
-            'price' => 'required|numeric|min:0',
-            'quantity' => 'required|integer|min:0',
-            'category' => 'required'
-        ]);
-
         $product = new Product();
-        $product->name = request()->input('name');
-        $product->description = request()->input('description');
-        $product->price = request()->input('price');
-        $product->quantity_in_stock = request()->input('quantity');
-        $product->category_id = request()->input('category');
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->quantity_in_stock = $request->input('quantity');
+        $product->category_id = $request->input('category');
         $product->user_id = auth()->id();
         $product->save();
 
         return redirect()
             ->route('dashboard.products.index')
-            ->with('success', 'Product created successfully.');
+            ->with('success', 'Product successfully created.');
     }
 
     /**
@@ -70,17 +64,34 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
+    public function edit(Product $product): Response
     {
-        //
+        abort_if(auth()->id() !== $product->user_id, 403);
+
+        $categories = Category::all();
+
+        return response()
+            ->view('dashboard.product.edit', compact('categories', 'product'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public function update(ProductStoreUpdateRequest $request, Product $product): RedirectResponse
     {
-        //
+        abort_if(auth()->id() !== $product->user_id, 403);
+
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->quantity_in_stock = $request->input('quantity');
+        $product->category_id = $request->input('category');
+        $product->user_id = auth()->id();
+        $product->save();
+
+        return redirect()
+            ->route('dashboard.products.index')
+            ->with('success', 'Product successfully updated.');
     }
 
     /**
--- resources/views/dashboard/product/edit.blade.php
+++ resources/views/dashboard/product/edit.blade.php
@@ -1,3 +1,101 @@
-<div>
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
-</div>
+@extends('layouts.app')
+
+@section('title')
+    Update Product
+@endsection
+
+@section('title-header')
+    Update Product
+@endsection
+
+@section('content')
+    <a href="{{ route('dashboard.products.index') }}" class="btn btn-primary">Back</a>
+
+    <div class="mt-4">
+        <form class="d-flex flex-column gap-3" method="POST" action="{{ route('dashboard.products.update', $product->id) }}">
+            @csrf
+            @method('PUT')
+
+            {{-- Name --}}
+            <div class="form-floating">
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                    required autocomplete="name" autofocus placeholder="Name" value="{{ old('name', $product->name) }}">
+                <label for="name">Name</label>
+
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            {{-- End Of Name --}}
+
+            {{-- Description --}}
+            <div class="form-floating">
+                <textarea id="description" class="form-control @error('description') is-invalid @enderror" name="description" required
+                    autocomplete="description" placeholder="Description" style="height: 200px;">{{ old('description', $product->description) }}</textarea>
+                <label for="description">Description</label>
+
+                @error('description')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            {{-- End Of Description --}}
+
+            {{-- Price --}}
+            <div class="form-floating">
+                <input id="price" type="number" class="form-control @error('price') is-invalid @enderror"
+                    name="price" required autocomplete="price" placeholder="Price" step="any"
+                    value={{ old('price', $product->price) }} />
+                <label for="price">Price</label>
+
+                @error('price')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            {{-- End Of Price --}}
+
+            {{-- Quantity In Stock --}}
+            <div class="form-floating">
+                <input id="quantity" type="number" class="form-control @error('quantity') is-invalid @enderror"
+                    name="quantity" required autocomplete="quantity" placeholder="Quantity In Stock"
+                    value="{{ old('quantity', $product->quantity_in_stock) }}" />
+                <label for="quantity">Quantity In Stock</label>
+
+                @error('quantity')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            {{-- End Of Quantity In Stock --}}
+
+            {{-- Categories --}}
+            <div class="form-floating">
+                <select class="form-select" id="category" name="category">
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}"
+                            {{ old('category', $category->id) == $product->category->id ? 'selected' : '' }}>
+                            {{ $category->name }}</option>
+                    @endforeach
+                </select>
+                <label for="category">Choose Category</label>
+
+                @error('category')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            {{-- End Of Categories --}}
+
+            {{-- Button Submit --}}
+            <button class="btn btn-primary w-100 py-2" type="submit">Save</button>
+            {{-- End Of Button Submit --}}
+        </form>
+    </div>
+@endsection
