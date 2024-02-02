2f00d7678223294c8757371a7759c8f1316561a4
Arya Priema <99aryaprima@gmail.com>
2024-02-02 20:16:39 +0700

    feat: create new order item

--- app/Http/Controllers/Dashboard/OrderItemController.php
+++ app/Http/Controllers/Dashboard/OrderItemController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OrderItemStoreUpdateRequest;
+use App\Models\Order;
 use App\Models\OrderItem;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -23,17 +26,42 @@ class OrderItemController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
-        //
+        return response()
+            ->view('dashboard.order-item.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(OrderItemStoreUpdateRequest $request)
     {
-        //
+        $product = Product::find($request->input('product_id'));
+
+        if ($request->input('quantity') > $product->quantity_in_stock) {
+            return redirect()
+                ->back()
+                ->withErrors(['quantity' => 'The quantity is greater than the quantity in stock.']);
+        }
+
+        $orderItem = new OrderItem();
+        $orderItem->quantity = $request->input('quantity');
+        $orderItem->unit_price = $product->price;
+        $orderItem->product_id = $request->input('product_id');
+        $orderItem->order_id = $request->input('order_id');
+        $orderItem->save();
+
+        $order = Order::find($request->input('order_id'));
+        $order->total_price = $product->price * $orderItem->quantity;
+        $order->save();
+
+        $product->quantity_in_stock = $product->quantity_in_stock - $orderItem->quantity;
+        $product->save();
+
+        return redirect()
+            ->route('dashboard.order-items.index')
+            ->with('success', 'OrderItem successfully created.');
     }
 
     /**
--- resources/views/dashboard/order-item/create.blade.php
+++ resources/views/dashboard/order-item/create.blade.php
@@ -1,3 +1,68 @@
-<div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
-</div>
+@extends('layouts.app')
+
+@section('title')
+    Create New Order Item
+@endsection
+
+@section('title-header')
+    Create New Order Item
+@endsection
+
+@section('content')
+    <a href="{{ route('dashboard.order-items.index') }}" class="btn btn-primary">Back</a>
+
+    <div class="mt-4">
+        <form class="d-flex flex-column gap-3" method="POST" action="{{ route('dashboard.order-items.store') }}">
+            @csrf
+
+            {{-- Quantity --}}
+            <div class="form-floating">
+                <input id="quantity" type="number" class="form-control @error('quantity') is-invalid @enderror"
+                    name="quantity" required autocomplete="quantity" autofocus placeholder="Quantity"
+                    value="{{ old('quantity') }}">
+                <label for="quantity">Quantity</label>
+
+                @error('quantity')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            {{-- End Of Quantity --}}
+
+            {{-- Product Id --}}
+            <div class="form-floating">
+                <input id="product_id" type="number" step="any"
+                    class="form-control @error('product_id') is-invalid @enderror" name="product_id" required
+                    autocomplete="product_id" autofocus placeholder="Product Id" value="{{ old('product_id') }}">
+                <label for="product_id">Product Id</label>
+
+                @error('product_id')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            {{-- End Of Product Id --}}
+
+            {{-- Order Id --}}
+            <div class="form-floating">
+                <input id="order_id" type="number" step="any"
+                    class="form-control @error('order_id') is-invalid @enderror" name="order_id" required
+                    autocomplete="order_id" autofocus placeholder="Order Id" value="{{ old('order_id') }}">
+                <label for="order_id">Order Id</label>
+
+                @error('order_id')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            {{-- End Of Order Id --}}
+
+            {{-- Button Submit --}}
+            <button class="btn btn-primary w-100 py-2" type="submit">Save</button>
+            {{-- End Of Button Submit --}}
+        </form>
+    </div>
+@endsection
